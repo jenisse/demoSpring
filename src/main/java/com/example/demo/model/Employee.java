06546365449b066ad8f5d2954dfdbed4752d6ed2
jenisse <jenisse.vereau@pucp.pe>
2018-07-31 13:16:34 -0500

    autogenerated value set to be on db side

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -13,6 +13,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -23,7 +24,7 @@ import javax.validation.constraints.NotNull;
 public class Employee {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     @NotNull
     @Column(name="nombre")
